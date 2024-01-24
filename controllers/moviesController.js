813cb1b0b3dc8e9c87ccfb92e77132318d355c79
Sayed <sayedfkabir@gmail.com>
2024-01-23 17:41:49 -0700

    added movie router code

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,5 +1,16 @@
 const Movie = require('../models/movie')
+ /*
+ router.post('/', movieCtrl.create)
 
+ router.get('/', movieCtrl.index)
+ 
+ router.get('/:id' ,movieCtrl.show)
+ 
+ router.get('/:movieId/performers/:performerId', movieCtrl.addPerformner)
+ */
+
+
+ 
 
 
 
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -2,6 +2,14 @@ const express = require('express')
 const router = express.Router()
 const movieCtrl = require('../controllers/moviesController')
 
+router.post('/', movieCtrl.create)
+
+router.get('/', movieCtrl.index)
+
+router.get('/:id' ,movieCtrl.show)
+
+router.get('/:movieId/performers/:performerId', movieCtrl.addPerformner)
+
 
 
 
